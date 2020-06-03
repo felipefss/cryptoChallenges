d2fece446fd2b97ac95e363aa9005adacf91a192
Felipe Santos <felipefss@gmail.com>
2020-06-03 17:18:54 +0100

    Initial commit
    Starting Set 1 - Hex to Base64

---
+++ set1/base64.js
@@ -0,0 +1,14 @@
+//input: 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+//output: SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+//Always operate on raw bytes, never on encoded strings. Only use hex and base64 for pretty-printing.
+
+const base64Table = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+  'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+  's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'];
+
+// Each Base64 digit represents exactly 6 bits of data
+// Three 8-bit bytes (a total of 24 bits) can be represented by four 6-bit Base64 digits.
+const hexToOctal = hex => {
+  const dec = parseInt(hex, 16);
+  return dec.toString(8);
+};
