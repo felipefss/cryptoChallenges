db61c55421229544bfdde3f42a1d3d75593805af
Felipe Santos <felipefss@gmail.com>
2020-06-05 11:47:44 +0100

    Finish Set1 - Challenge 3
    Single-byte XOR cipher

---
+++ set1/03_singleByteXOR.js
@@ -0,0 +1,46 @@
+const addScore = (obj, x) => {
+  obj[x] = !obj[x] ? 1 : obj[x] + 1;
+};
+
+const findMaxScore = score => {
+  const scObj = {
+    max: -1,
+    key: ''
+  };
+
+  for (let c in score) {
+    if (score[c] > scObj.max) {
+      scObj.max = score[c];
+      scObj.key = c;
+    }
+  }
+
+  return scObj.key;
+};
+
+const decrypt = (str) => {
+  //etaoin shrdlu
+  const score = {};
+
+  const buf = Buffer.from(str, 'hex');
+  const bufStr = buf.toString();
+
+  for (let c of bufStr) {
+    addScore(score, c);
+  }
+
+  const key = findMaxScore(score);
+  const keyBuf = Buffer.from(key);
+  const xorArray = [];
+
+  for (let b of buf.values()) {
+    xorArray.push(b ^ keyBuf[0]);
+  }
+
+  return Buffer.from(xorArray).toString()
+};
+
+const res = decrypt('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736');
+console.log(res)
+
+//1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736
